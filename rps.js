09579f2f884e9d72c61979f934d9514f67d45d76
Ahmed Nabi Khan <26043220+ahmednabik@users.noreply.github.com>
2023-05-28 03:28:05 +0500

    write first version of working logic for the game.

--- rps.js
+++ rps.js
@@ -1,10 +1,55 @@
+// event listener for Night mode
 let toggleSwitch = document.getElementById('switch');
 toggleSwitch.addEventListener("change", toggleDayNight);
 
+//event listener for "Reset" button
+let resetButton = document.getElementById('button-28');
+resetButton.addEventListener("click", clearResults);
 
+let currentTurnNumber = 1; //initialise game count
+startGame();
 
+function startGame() {
+    console.log("game started")
+    clearResults();
+    let clickArea = document.querySelector('.game');
+    clickArea.addEventListener("click", (card) => {
+        let firstPlayer = card.target.parentNode.getAttribute('data-value');
+        let result = calculateResult(firstPlayer, currentTurnNumber); //return either W or L
+        populateResult(result);
+        if (currentTurnNumber > 5) {
+            gameOver();
+        }
+        else {
+            currentTurnNumber = currentTurnNumber + 1;
+        }
+    });
+}
+function calculateResult(firstPlayer, currentTurnNumber) {
+    let gameOptions = ['rock', 'paper', 'scissor'];
+    let secondPlayer = gameOptions[Math.floor(Math.random() * 3)];
+    console.log(secondPlayer);
+    if ((firstPlayer == "rock" && secondPlayer == "scissor") || (firstPlayer == "scissor" && secondPlayer == "paper") || (firstPlayer == "paper" && secondPlayer == "rock")) {
+        return "W";
+    }
+    else {
+        return "L";
+    }
 
-
+}
+function populateResult(result) {
+    let historyCards = document.querySelectorAll('.history-card');
+    if (currentTurnNumber >= 1 && currentTurnNumber <= 5) {
+        historyCards[currentTurnNumber - 1].innerHTML = result;
+    }
+}
+function clearResults() {
+    let historyCards = document.querySelectorAll('.history-card');
+    historyCards.forEach(card => {
+        card.innerHTML = " ";
+    });
+    currentTurnNumber = 1;
+}
 function toggleDayNight() {
 
     /* Toggle HTML elements */
@@ -17,16 +62,21 @@ function toggleDayNight() {
     /* Toggle images */
     let toggleImg = document.getElementsByClassName('img-toggle');
     let toggleImgArray = Array.from(toggleImg);
-    console.log(toggleImgArray);
 
-        if (!toggleSwitch.checked) {
-            toggleImgArray[0].src = "img/rock.png";
-            toggleImgArray[1].src = "img/paper.png";
-            toggleImgArray[2].src = "img/scissor.png";
-        }
-        else {
-            toggleImgArray[0].src = "img/rock-dark.png";
-            toggleImgArray[1].src = "img/paper-dark.png";
-            toggleImgArray[2].src = "img/scissor-dark.png";
-        }
+
+    if (!toggleSwitch.checked) {
+        toggleImgArray[0].src = "img/rock.png";
+        toggleImgArray[1].src = "img/paper.png";
+        toggleImgArray[2].src = "img/scissor.png";
+    }
+    else {
+        toggleImgArray[0].src = "img/rock-dark.png";
+        toggleImgArray[1].src = "img/paper-dark.png";
+        toggleImgArray[2].src = "img/scissor-dark.png";
+    }
+}
+function gameOver() {
+    alert("game over");
+    clearResults();
+
 }
